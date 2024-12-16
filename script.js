5d68946b522d4e4c459b6b1ffa069aaf24107837
Irenygeorge <irenymary15@gmail.com>
2024-12-16 22:23:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+// Console? "Developer tool"
+// 1- Catch Errors
+// 2- Try Your Codes
+
+// --------------
+
+// Variable متغير
+    // فايدته ايه؟
+    // var=>variable
+    // var ramses="ده عباره عن ميدان رئيسي في مصر مسمي علي اسم ملك فرعوني"
+    // var userName="ireny"
+    // var gender="female" 
+// بيتكتب ازاي؟
+// Outputs 
+// Style=>CSS
+// Script=>Java Script
+// Documents=>HTML
+
+// document.write(ramses);
+// console.log(ramses);
+
+
+// Interactions
+//  alert(ramses);بيعمل تنبيه لشئ معين في الصفحه
+//  var age= prompt(" عندك كام سنه"); 
+//  confirm("تأكيد العمليه");
+
+
+
+// Primitive Data Types
+// 1-String => "" or''
+// 2-Numbers
+// 3-Boolean => True or False
+
+
+// لو الباب فتوح ادخل
+// لو الباب مقفول؟
+
+//  var door="open";
+// var door ="close"
+
+//  if(door=="open"){
+//      document.write("تقدر تدخل ");
+// }else{
+//      document.write("خبط علي الباب و افتح");
+// }
+
+
+// ---if Condition---
+// if=>Keyword لو
+// () Condition الشرط
+
+// ---Comparisor Operator---
+// ( = ) variable
+// ( == )بتتاكد ان القيمه بتساوي قيمه تانيه 
+// ( === ) بتتاكد ان القيمه بتساوي قيمه تانيه
+
+
+
+
+ var degree = prompt("ما هي الدرجه اللتي حصلت عليها؟")
+   if (degree >= 50) {
+    document.write("لقد نجحت")
+ }else{
+    document.write("حاول مره اخري");
+  }
